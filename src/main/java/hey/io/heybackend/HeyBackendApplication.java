fdc2850c0d9d7af387645cceeadea817a30b022c
MangoSteen0903 <sjlee990129@gmail.com>
2024-02-14 17:11:19 +0900

    init project

---
+++ src/main/java/hey/io/heybackend/HeyBackendApplication.java
@@ -0,0 +1,13 @@
+package hey.io.heybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeyBackendApplication.class, args);
+    }
+
+}
