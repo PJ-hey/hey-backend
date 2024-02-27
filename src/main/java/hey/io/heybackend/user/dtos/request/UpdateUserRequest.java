688eb1637b2db9116116d8f69afdc76930a7320a
seeeeeeong <lsinsung@gmail.com>
2024-02-27 23:16:36 +0900

    feat: swagger 설정

--- src/main/java/hey/io/heybackend/user/dtos/request/UpdateUserRequest.java
+++ src/main/java/hey/io/heybackend/user/dtos/request/UpdateUserRequest.java
@@ -1,5 +1,6 @@
 package hey.io.heybackend.user.dtos.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateUserRequest {
 
+    @Schema(description = "닉네임", example = "홍길동", required = true)
     private String nickName;
+
+    @Schema(description = "비밀번호", example = "abc!1234", required = true)
     private String password;
 
 
